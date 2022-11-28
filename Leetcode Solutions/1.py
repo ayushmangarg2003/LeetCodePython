feaa65ea3058f62a311071bcc675bb63183bc93e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-11-28 19:30:22 +0530

    Update 1.py

--- Leetcode Solutions/1.py
+++ Leetcode Solutions/1.py
@@ -5,8 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range (len(nums)):
-            for j in range(i+1,len(nums)):
-                sum = nums[i] + nums[j]
-                if sum == target:
-                    return(i,j)
+        for i in range(len(nums)):
+            for j in range(i+1 , len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        
