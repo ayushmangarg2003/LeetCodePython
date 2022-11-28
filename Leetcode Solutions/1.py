34203ccfe1e8f3f187c9ea789cbbdbfb7d505747
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-11-28 19:26:51 +0530

    Add files via upload

---
+++ Leetcode Solutions/1.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range (len(nums)):
+            for j in range(i+1,len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return(i,j)
