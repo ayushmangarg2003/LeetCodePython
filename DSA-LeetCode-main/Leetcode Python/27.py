5fe95e01b936e2912a6e0851435958b91cf1b9a4
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-12-07 11:19:30 +0530

    Add files via upload

---
+++ DSA-LeetCode-main/Leetcode Python/27.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
+        return k
