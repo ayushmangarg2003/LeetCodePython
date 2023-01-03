dd9ab96f8f09a38ee4f3043483b2d8d33cf4d36e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-03 10:30:58 +0530

    Create 217.py

---
+++ DSA-LeetCode-main/Leetcode Python/217.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
