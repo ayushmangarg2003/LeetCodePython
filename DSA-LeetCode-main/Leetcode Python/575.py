8f906a6e717305d9cc75040ee146b27f7c2c313e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-12 12:12:19 +0530

    Create 575.py

---
+++ DSA-LeetCode-main/Leetcode Python/575.py
@@ -0,0 +1,5 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int: 
+        if len(set(candyType))< len(candyType)//2:
+            return len(set(candyType))
+        return len(candyType)//2
