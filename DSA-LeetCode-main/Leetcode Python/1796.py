fdba7f3f671b0409de6bd3176c320c882ff5a0f4
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-16 11:00:00 +0530

    Create 1796.py

---
+++ DSA-LeetCode-main/Leetcode Python/1796.py
@@ -0,0 +1,9 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        digits = set()
+        s = list(set(s))
+        for letter in s:
+            if letter.isdigit():
+                digits.add(letter)
+        digits = sorted(list(digits))
+        return -1 if len(digits) < 2 else digits[-2]
