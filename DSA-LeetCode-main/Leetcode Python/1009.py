341898de05cc674fae2900881b82fe3fde7cd7b9
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-02-11 16:57:13 +0530

    Create 1009.py

---
+++ DSA-LeetCode-main/Leetcode Python/1009.py
@@ -0,0 +1,11 @@
+class Solution:
+    def bitwiseComplement(self, num: int) -> int:
+        s=bin(num).replace('0b','')
+        t=''
+        for i in s:
+            if i=='0':
+                t+='1'
+            else:
+                t+='0'
+        return int(t,2)
+3
