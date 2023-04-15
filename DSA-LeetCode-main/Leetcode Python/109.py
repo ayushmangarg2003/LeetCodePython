7ddfa1eb6fa12ea5c3b5cd396541047e6fe19f30
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-04-15 10:59:23 +0530

    Create 109.py

---
+++ DSA-LeetCode-main/Leetcode Python/109.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        slow = fast = head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        root = TreeNode(slow.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(slow.next)
+        return root
