3088509f85977fb1e50f97ccdba8b7b30490a1b5
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-05-26 17:49:45 +0530

    Update 1346.py

--- LeetcodePython/1346.py
+++ LeetcodePython/1346.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        if 0 in arr:
+            arr.remove(0)
+        for i in arr:
+            if i*2 in arr:
+                return True
+        return False
