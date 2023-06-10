e2f7ad346b88de169b64cc40d92a2ffec110a462
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-06-10 14:27:08 +0530

    Create 1137.py

---
+++ LeetcodePython/1137.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a=0
+        b=1
+        c=1
+        if n==0 or n==1:
+            return n
+        for i in range(2,n):
+            d=a+b+c
+            a=b
+            b=c
+            c=d
+        return c
