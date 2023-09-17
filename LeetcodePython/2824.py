8ca2a069de9c681efe17fe7f5a9f65eca9b04103
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-09-17 21:30:24 +0530

    Create 2824.py

---
+++ LeetcodePython/2824.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        ans = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] < target:
+                    ans+=1
+        return ans
