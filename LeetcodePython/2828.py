e5a0207d9d7beb688e55e1b5fe5c8fc958b0a549
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-09-17 21:38:43 +0530

    Create 2828.py

---
+++ LeetcodePython/2828.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        if len(words) != len(s):
+            return False
+        for i in range(len(words)):
+            if words[i][0]!=s[i]:
+                return False
+        return True
