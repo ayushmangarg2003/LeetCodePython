03153644ba4f10d4be1cc178d2bce3667a49fc5b
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-12-19 23:59:41 +0530

    Create 164.py

---
+++ LeetcodePython/164.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums)<2:
+            return 0
+        ans = 0
+        nums.sort()
+        for i in range(len(nums)-1):
+            ans = max(ans, nums[i+1]-nums[i])
+        return ans
