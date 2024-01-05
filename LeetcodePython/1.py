9fc99f568c0eaf724c87a528c96a5185e7179c37
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-01-05 19:55:22 +0530

    Update 1.py

--- LeetcodePython/1.py
+++ LeetcodePython/1.py
@@ -1,12 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i+1 , len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prev = {}
+        for k,v in enumerate(nums):
+            diff = target - v
+            if diff in prev:
+                return [prev[diff],k]
+            prev[v]=k
