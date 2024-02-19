b1fefeaec95698cf3f34a78699abc55bd316bf7c
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-02-19 19:46:59 +0530

    Update 36.py

--- LeetcodePython/36.py
+++ LeetcodePython/36.py
@@ -8,10 +8,11 @@ class Solution:
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
+                elif (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[r//3, c//3]):
                     return False
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
+                squares[r//3,c//3].add(board[r][c])
         return True
-
