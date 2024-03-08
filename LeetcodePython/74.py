fe00457f857a812aec1629a933ba2aeb1bb16e92
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-03-08 17:13:02 +0530

    Update 74.py

--- LeetcodePython/74.py
+++ LeetcodePython/74.py
@@ -1,26 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        top , bot = 0 , len(matrix)-1
-        while top <= bot:
+        rows,cols = len(matrix),len(matrix[0])
+        top, bot = 0 , rows-1
+
+        while top<=bot:
             row = (top+bot)//2
             if target > matrix[row][-1]:
-                top = row+1
+                top= row+1
             elif target < matrix[row][0]:
-                bot = row-1
+                bot= row-1
             else:
                 break
-                
-        if not (top<=bot):
-            return 0
+        if not top<=bot:
+            return False
+        
         row = (top+bot)//2
-        lef , rig = 0 , len(matrix[0])-1
-        while lef<=rig:
-            mid = (lef+rig)//2
-            if target > matrix[row][mid]:
-                lef = mid+1
-            elif target < matrix[row][mid]:
-                rig = mid-1
+        l,r = 0, cols-1
+        while l<=r:
+            m = (l+r)//2
+            if matrix[row][m] < target:
+                l = m+1
+            elif matrix[row][m] > target:
+                r = m-1
             else:
-                return 1
-        return 0
+                return True
+        return False
