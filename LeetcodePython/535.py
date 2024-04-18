da25c458a5dec1c0304a16cc7bf85c341cff6fa0
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-04-18 17:17:44 +0530

    Create 535.py

---
+++ LeetcodePython/535.py
@@ -0,0 +1,10 @@
+class Codec:
+    def __init__(self):
+        self.urls = []
+
+    def encode(self, longUrl):
+        self.urls.append(longUrl)
+        return 'http://tinyurl.com/' + str(len(self.urls) - 1)
+
+    def decode(self, shortUrl):
+        return self.urls[int(shortUrl.split('/')[-1])]
