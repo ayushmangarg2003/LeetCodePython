532ea7f0b57ef181eb134944153c0e94bbedd05d
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-04-19 17:59:50 +0530

    Create 3099.py

---
+++ LeetcodePython/3099.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        sum = 0
+        for i in str(x):
+            sum+=int(i)
+        if x % sum == 0:
+            return sum
+        return -1
