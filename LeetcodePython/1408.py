6b59a0e15c5045360f1ad6ffda4a1413c349dc98
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-01-07 16:55:51 +0530

    Update 1408.py

--- LeetcodePython/1408.py
+++ LeetcodePython/1408.py
@@ -1,5 +1,16 @@
+# This one worked but has is own edge case for ex ['leet','code','leetc']
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         arr = ' '.join(words)
         subStr = [i for i in words if arr.count(i) >= 2]
         return subStr
+
+#  Better Solution
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if words[i] in words[j] and i!=j:
+                    ans.append(words[i])
+        return list(set(ans))
