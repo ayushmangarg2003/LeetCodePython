6ad0d553314888355310d6697a9402565fac0368
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-01-08 13:00:46 +0530

    Update 3042.py

--- LeetcodePython/3042.py
+++ LeetcodePython/3042.py
@@ -12,3 +12,14 @@ class Solution:
                 if check(words[i], words[j]):
                     ans+=1
         return ans
+
+# 2nd sol
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        ans = 0
+
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if words[j].find(words[i]) == 0 and words[j].rfind(words[i]) == (len(words[j]) - len(words[i])):
+                    ans+=1
+        return ans
