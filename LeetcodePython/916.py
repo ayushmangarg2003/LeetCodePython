5d3a212288c60656f3a38ec18b1d4461a7b7c514
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-01-10 12:08:27 +0530

    Create 916.py

---
+++ LeetcodePython/916.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordSubsets (self, words1: List[str], words2: List[str]) -> List[str]:
+        count_2 = defaultdict(int)
+        for w in words2:
+            count_w = Counter(w)
+            for c, cnt in count_w.items():
+                count_2[c] = max(count_2[c], cnt)
+        res = []
+        for w in words1:
+            count_w = Counter(w)
+            flag = True
+            for c, cnt in count_2.items():
+                if count_w[c] < cnt:
+                    flag = False
+                    break
+            if flag:
+                res.append(w)
+        return res
